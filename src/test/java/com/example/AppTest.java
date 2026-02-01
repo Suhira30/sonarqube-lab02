555cf4d28c9f394c7e5c2f131b815e5f57ec83e7
Suhira30 <suhirabalarajan@gmail.com>
2026-02-02 00:11:28 +0530

    Fix quality issues

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void testMain() {
+        // Execute main method to verify it runs without crashing (exceptions are caught
+        // internally)
+        App.main(new String[] {});
+    }
+}
