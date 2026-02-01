eb276c37f0c6993199705f7c3b700490a5006ce7
Suhira30 <suhirabalarajan@gmail.com>
2026-02-01 17:34:18 +0530

    Fix quality issues

--- src/test/java/main/java/com/example/CalculatorTest.java
+++ src/test/java/main/java/com/example/CalculatorTest.java
@@ -8,5 +8,6 @@ class CalculatorTest {
     @Test
     void adds() {
         assertEquals(15, new Calculator().calculate(10, 5, "add"));
+        assertEquals(5, new Calculator().calculate(10, 5, "sub"));
     }
 }
